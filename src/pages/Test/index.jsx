0cf717d335fedc2e5f6eff119f6eda65afbd1fc4
cfigueiroa <cfigueiroa@gmail.com>
2022-12-19 17:38:12 -0300

    test: JWT debug

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,12 +1,26 @@
-import { IonIcon } from "@ionic/react";
-import { trashOutline, checkmark } from "ionicons/icons";
-
+// import { IonIcon } from "@ionic/react";
+// import { trashOutline, checkmark } from "ionicons/icons";
+import useMyContext from "../../components/Context";
 export default function Test() {
+  const { user } = useMyContext();
+  // Set the JWT
+  const jwt = user.token;
+
+  // Split the JWT into its three parts
+  const [header, payload, signature] = jwt.split(".");
+
+  // Base64-decode the header and payload
+  const decodedHeader = JSON.parse(atob(header));
+  const decodedPayload = JSON.parse(atob(payload));
+
   return (
     <div>
-      Test
-        <IonIcon icon={checkmark} />
-        <IonIcon icon={trashOutline} />
+      <h1>JWT Debugger</h1>
+      {`header: ${header}`} <br />
+      {`decodedHeader : ${JSON.stringify(decodedHeader)}`} <br />
+      {`payload: ${payload}`} <br />
+      {`decodedPayload: ${JSON.stringify(decodedPayload)}`} <br />
+      {`signature: ${signature}`}
     </div>
   );
 }
